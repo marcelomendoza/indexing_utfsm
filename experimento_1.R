bf7b971acbaa9db9da31bffda8077cedea50d562
Mauricio Araya <mauricio@mac.local>
2015-04-14 23:11:58 -0300

    LICENSED

--- experimento_1.R
+++ experimento_1.R
@@ -1,3 +1,20 @@
+#This file is part of ChiVO
+#Copyright (C) Gabriel Candia, Marcelo Mendoza
+#
+#This program is free software; you can redistribute it and/or
+#modify it under the terms of the GNU General Public License
+#as published by the Free Software Foundation; either version 2
+#of the License, or (at your option) any later version.
+#
+#This program is distributed in the hope that it will be useful,
+#but WITHOUT ANY WARRANTY; without even the implied warranty of
+#MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#GNU General Public License for more details.
+#
+#You should have received a copy of the GNU General Public License
+#along with this program; if not, write to the Free Software
+#Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+#
 experimento_1 <- function(brush, w, offset) {
   x <- thresh(nuc, w, w, offset)
   x = opening(x, makeBrush(brush, shape='disc'))
