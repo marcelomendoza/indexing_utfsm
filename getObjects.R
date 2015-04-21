baf50c4fa870ff2b57ac31881b856fce941b5a60
marcelomendoza <mmendoza@inf.utfsm.cl>
2015-04-21 14:01:54 -0300

    Create getObjects.R

---
+++ getObjects.R
@@ -0,0 +1,35 @@
+#This file is part of ChiVO, the Chilean Virtual Observatory
+#A project sponsored by FONDEF (D11I1060)
+#Copyright (C) 2015 Universidad Tecnica Federico Santa Maria Mauricio Solar
+#                                                            Marcelo Mendoza
+#                   Universidad de Chile                     Diego Mardones
+#                   Pontificia Universidad Catolica          Karim Pichara
+#                   Universidad de Concepcion                Ricardo Contreras
+#                   Universidad de Santiago                  Victor Parada
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+
+#
+getObjects <- function(im) {
+  y <- im
+  #y <- im$imDat
+  #image(y, zlim=c(1100,1200), col=gray.colors(256))
+  x <- thresh(y, 10, 10, 0)
+  x = opening(x, makeBrush(12, shape='disc'))
+  x <- bwlabel(x)
+  fts <- computeFeatures.shape(x)
+  xy <- computeFeatures.moment(x)[, c('m.cx','m.cy')]
+  return(list(xy, fts))
+}
