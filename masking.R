f36e734a124ac919a70f54fdbaaf849d3437874e
marcelomendoza <mmendoza@inf.utfsm.cl>
2015-04-21 12:20:18 -0300

    Create masking.R

---
+++ masking.R
@@ -0,0 +1,37 @@
+#This file is part of ChiVO, the Chilean Virtual Observatory
+#A project sponsored by FONDEF (D11I1060)
+#Copyright (C) 2015 Universidad Tecnica Federico Santa Maria Mauricio Solar
+#                                                            Marcelo Mendoza
+#                   Universidad de Chile                     Diego Mardones
+#                   Pontificia Universidad Catolica          Karim Pichara
+#                   Universidad de Concepcion                Ricardo Contreras
+#                   Universidad de Santiago                  Victor Parada
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+
+#
+masking <- function(boxing,pixel) {
+  n1 = length(boxing)
+  n2 = length(pixel)
+  if (n1==n2) {
+    n <- n1
+    output <- array(0.0, dim=c(n))
+    for (i in 1:n){
+      output[i] <- boxing[i]*pixel[i]
+    }
+    return(output)
+  }
+  else return(0)
+}
